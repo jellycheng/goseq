5080a6816f120080432ce480a283b89d1a51f544
程金盛 <42282367@qq.com>
2024-05-22 21:58:29 +0800

    add redis

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/jellycheng/goseq
 
 go 1.14
+
+require (
+	github.com/go-redis/redis/v8 v8.11.4 // indirect
+	github.com/jellycheng/gosupport v1.0.4 // indirect
+)
---
+++ redis.go
@@ -0,0 +1,35 @@
+package goseq
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"github.com/jellycheng/gosupport"
+)
+
+var ctx = context.Background()
+var rdbObjMap = make(map[string]*redis.Client)
+
+func GetRedisClient(cfg RedisCfg) *redis.Client {
+	k := gosupport.Md5V1(fmt.Sprintf("%s%s%s", cfg.Host, cfg.Port, cfg.Password))
+	if r, ok := rdbObjMap[k]; ok {
+		return r
+	}
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", cfg.Host, cfg.Port),
+		Password: cfg.Password,
+		DB:       gosupport.Str2Int(cfg.Db),
+	})
+	rdbObjMap[k] = rdb
+	return rdb
+}
+
+func SetKeyValue(rdb *redis.Client, key, value string) error {
+	err := rdb.Set(ctx, key, value, 0).Err()
+	return err
+}
+
+func GetKeyValue(rdb *redis.Client, key string) string {
+	val, _ := rdb.Get(ctx, key).Result()
+	return val
+}
